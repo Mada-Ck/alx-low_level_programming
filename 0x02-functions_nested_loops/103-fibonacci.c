c6865fe40d735562c216b3e6d5abb083a8ab2532
Mada-Ck <madalitsokalua@gmail.com>
2023-12-19 11:58:53 -0800

    0x02-functions_nested_loops

--- 0x02-functions_nested_loops/103-fibonacci.c
+++ 0x02-functions_nested_loops/103-fibonacci.c
@@ -7,35 +7,35 @@
  */
 int main(void)
 {
-    // Initialize variables
+    /* Initialize variables */
     int i;
     unsigned long int j, k, next, sum;
 
-    // Initialize the first two numbers in the Fibonacci sequence
+    /* Initialize the first two numbers in the Fibonacci sequence */
     j = 1;
     k = 2;
-    // Initialize the sum to zero
+    /* Initialize the sum to zero */
     sum = 0;
 
-    // Loop to find and sum even-valued Fibonacci terms below 4,000,000
-    for (i = 1; i <= 33; ++i)  // 33 iterations are sufficient to reach 4,000,000
+    /* Loop to find and sum even-valued Fibonacci terms below 4,000,000 */
+    for (i = 1; i <= 33; ++i)  /* 33 iterations are sufficient to reach 4,000,000 */
     {
-        // Check if the current Fibonacci term is even and below 4,000,000
+        /* Check if the current Fibonacci term is even and below 4,000,000 */
         if (j < 4000000 && (j % 2) == 0)
         {
-            // Add the even term to the sum
+            /* Add the even term to the sum */
             sum = sum + j;
         }
-        // Calculate the next term in the Fibonacci sequence
+        /* Calculate the next term in the Fibonacci sequence */
         next = j + k;
-        // Update j and k for the next iteration
+        /* Update j and k for the next iteration */
         j = k;
         k = next;
     }
 
-    // Print the sum of even-valued Fibonacci terms
+    /* Print the sum of even-valued Fibonacci terms */
     printf("%lu\n", sum);
 
-    // Return 0 to indicate successful completion
+    /* Return 0 to indicate successful completion */
     return (0);
 }
